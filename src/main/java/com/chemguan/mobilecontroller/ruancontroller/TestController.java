7d8bf1b552ccfd8edd431b2811c58ab6efeeb594
ruanhongliang <1274769751@qq.com>
2019-03-20 18:46:01 +0800

    ruan

---
+++ src/main/java/com/chemguan/mobilecontroller/ruancontroller/TestController.java
@@ -0,0 +1,24 @@
+package com.chemguan.mobilecontroller.ruancontroller;
+
+import com.chemguan.business.core.results.Result;
+import com.chemguan.business.core.results.ResultGenerator;
+import com.chemguan.entity.TsActivity;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @Title: TsUserModelController
+ * @ProjectName
+ * @Description: TODO
+ * @author 
+ * @date Wed Mar 20 11:32:52 CST 2019
+ */
+@Controller
+@RequestMapping("mobile")
+public class TestController {
+
+
+
+}
